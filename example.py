f55875232bc360a528aaba579afa8ce2d8f03d9e
Benito <marcote@jive.eu>
2021-04-01 10:17:49 +0200

    Added subtract

--- example.py
+++ example.py
@@ -8,3 +8,11 @@ def test_add():
     assert add('space', 'ship') == 'spaceship'
     assert abs(add(0.1, 0.2)/0.3 - 1) < 1e-5
 
+def subtract(a, b):
+    return a - b
+
+
+def test_subtract():
+    assert subtract(2, 3) == -1
+    assert subtract(0, 0) == 0
+    assert abs(subtract(5.5, 3.3)/(5.5-3.3) - 1) < 1e-5
